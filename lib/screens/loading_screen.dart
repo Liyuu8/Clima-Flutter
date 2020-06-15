2cef62057d3f2bbbea339826c82038523a0da24a
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2020-06-15 23:54:07 +0900

    Implement the loading spinner

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 // services
 import '../services/location.dart';
@@ -44,6 +45,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitFadingFour(
+          color: Colors.white,
+          size: 200.0,
+        ),
+      ),
+    );
   }
 }
